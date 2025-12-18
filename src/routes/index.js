f8493216804164136be5fd6cbcfd218630bddc62
adisalaras <132965173+adisalaras@users.noreply.github.com>
2025-12-18 16:51:04 +0700

    feat: add crud users (#4)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,9 @@ const Express = require('express');
 const router = Express.Router();
 
 const authRoutes = require('../services/auth/auth.route');
+const userRoutes = require('../services/users/users.route');
 
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
